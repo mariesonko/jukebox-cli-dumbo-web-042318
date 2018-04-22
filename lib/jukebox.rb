c8f231e7455d7621e6bd960328f79f12386549f4
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 20:53:40 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,9 +15,9 @@ def help
   I accept the following commands
   - help: displays this help message
   - list: displays a list of songs you can play
-  - play: let's you choose a song to play
+  - play: lets you choose a song to play
   - exit: exits this program
   HELP
 
   puts help
-end 
+end
