bea8e881e0ab7c263dd330ad15fec35e6ff1511e
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 20:55:01 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,7 @@ songs = [
 
 def help
   help = <<- HELP
-  I accept the following commands:
+  I accept the following commands 
   - help : displays this help message
   - list : displays a list of songs you can play
   - play : lets you choose a song to play
