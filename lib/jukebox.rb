fb21b69f6a5c9cc6890273eed0f47ab9551e3897
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 21:00:33 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,33 +9,3 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
-
-def help
-  help = <<-HELP
- I accept the following commands:
- - help : displays this help message
- - list : displays a list of songs you can play
- - play : lets you choose a song to play
- - exit : exits this program
- HELP
-   puts help
- end
-end
-
-def list(songs)
-  songs.each_with_index { |item, index|
-    puts "#{index+1}. #{item}" }
-end
-
-def play(songs)
-  puts "Please enter a song name or number:"
-  user_response = gets.downcase.chomp
-
-  if (1..9).to_a.include?(user_response.to_i)
-    puts "Playing #{songs[user_response.to_i - 1]}"
-    elsif songs.include?(user_response)
-    puts "Playing #{user_response}"
-  else
-    puts "Invalid input, please try again"
-  end
-end
